43668fa51c376a62ed891e3ad8fdb0b715973527
Armin <sanarena@gmail.com>
2020-12-29 23:42:53 +0800

    fix(ssl-passthrough): SSL passthrough parameter disable proxy protocol. so we need to disable it.

--- nginx.tf
+++ nginx.tf
@@ -145,7 +145,7 @@ resource "kubernetes_role" "nginx" {
   rule {
     api_groups = [""]
     resources  = ["configmaps"]
-    verbs      = ["create"]
+    verbs      = ["create", "update"]
   }
 
   rule {
@@ -264,7 +264,7 @@ resource "kubernetes_deployment" "nginx" {
             "--udp-services-configmap=$(POD_NAMESPACE)/${kubernetes_config_map.nginx_udp.metadata.0.name}",
             "--publish-service=$(POD_NAMESPACE)/${var.name}",
             "--enable-ssl-chain-completion=true",
-            "--enable-ssl-passthrough",
+            # "--enable-ssl-passthrough",
             "--election-id=${var.name}-leader",
             "--ingress-class=nginx",
           ]
--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,6 @@
 output "lb_address" {
   value       = length(kubernetes_service.lb.load_balancer_ingress[0].ip) > 0 ? kubernetes_service.lb.load_balancer_ingress[0].ip : kubernetes_service.lb.load_balancer_ingress[0].hostname
+  # value       = "example.org"
   description = "The hostname of the LB created by kubernetes"
 }
 
