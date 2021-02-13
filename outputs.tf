52e0a4e5c428250ca27faa0cde2464e1f467b628
Armin <sanarena@gmail.com>
2021-02-14 02:02:23 +0800

    feat(kubernetes): Upgrade to kubernetes module v2.0.2

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "lb_address" {
-  value = length(kubernetes_service.lb.load_balancer_ingress) > 0 ? (length(kubernetes_service.lb.load_balancer_ingress[0].ip) > 0 ? kubernetes_service.lb.load_balancer_ingress[0].ip : kubernetes_service.lb.load_balancer_ingress[0].hostname) : "127.0.0.1"
+  value = length(kubernetes_service.lb.status.0.load_balancer) > 0 ? length(kubernetes_service.lb.status[0].load_balancer[0].ingress) > 0 ? (length(kubernetes_service.lb.status[0].load_balancer[0].ingress[0].ip) > 0 ? kubernetes_service.lb.status[0].load_balancer[0].ingress[0].ip : kubernetes_service.lb.status[0].load_balancer[0].ingress[0].hostname) : "127.0.0.1" : "127.0.0.1"
   # value       = "example.org"
   description = "The hostname of the LB created by kubernetes"
 }
