a86bdc2e2a1bf06737f93df981960831fae633fb
fst0 <fs@parasyntax.org>
2022-11-03 02:05:43 +0100

    Update nginx.tf

--- nginx.tf
+++ nginx.tf
@@ -1,7 +1,7 @@
 // https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.29.0/deploy/static/mandatory.yaml
 resource "kubernetes_namespace" "nginx" {
   metadata {
-    name = var.name
+    name = var.namespace
 
     labels = {
       "app.kubernetes.io/name"       = var.name
